dc6efa6de75adb7dc0efd221b73f7a180652b739
mhrezaei1 <118566365+mhrezaei1@users.noreply.github.com>
2025-07-11 17:41:11 +0000

    Deploying to gh-pages from @ mhrezaei1/mhrezaei1.github.io@ed8e5430e2400834671e96156251878b6bbccaf7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "I have three documents that might be of interest to you; My Resume, My CV, and My Coursework.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
